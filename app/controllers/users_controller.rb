5acd2917d337eba182cfe19f0d13bb663a6e8353
ZaliyaD <zaliya.dukina@gmail.com>
2022-10-14 18:05:28 +0300

    Authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def create
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.create(user_params)
+    session[:user_id] = @user.id
+
+    redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+  end
+
+  def new
+    session[:current_time] = Time.now
+    @user = User.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
       patch :hide
     end
   end
+
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create]
 end
