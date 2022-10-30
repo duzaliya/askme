e964303a90ff93e406d888852cb950358b879cfc
ZaliyaD <zaliya.dukina@gmail.com>
2022-10-30 19:01:23 +0300

    Question author

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,15 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[show edit update destroy hide]
+  before_action :ensure_current_user, only: %i[edit update destroy hide]
+  before_action :set_question_for_current_user, only: %i[edit update destroy hide]
 
   def create
+    question_params = params.require(:question).permit(:body, :user_id)
     @question = Question.new(question_params)
 
+    @question.author = current_user
+
     if @question.save
-      redirect_to question_path(@question), notice: 'Новый вопрос создан!'
+      redirect_to user_path(@question.user), notice: 'Новый вопрос создан!'
     else
       flash.now[:alert] = 'Вы неправильно заполнили поля формы вопроса'
       render :new
@@ -13,8 +17,9 @@ class QuestionsController < ApplicationController
   end
 
   def update
+    question_params = params.require(:question).permit(:body, :answer)
     if @question.update(question_params)
-      redirect_to question_path(@question), notice: 'Изменения сохранены'
+      redirect_to user_path(@question.user), notice: 'Изменения сохранены'
     else
       flash.now[:alert] = 'Вы неправильно заполнили поля формы вопроса'
       render :new
@@ -22,12 +27,14 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+    @user = @question.user
     @question.destroy
 
-    redirect_to questions_path, notice: 'Вопрос удален!'
+    redirect_to user_path(@user), notice: 'Вопрос удален!'
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def index
@@ -36,7 +43,8 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
+    @user = User.find(params[:user_id])
+    @question = Question.new(user: @user)
   end
 
   def edit
@@ -50,11 +58,11 @@ class QuestionsController < ApplicationController
 
   private
 
-  def question_params
-    params.require(:question).permit(:body, :user_id)
+  def ensure_current_user
+    redirect_with_alert unless current_user.present?
   end
 
-  def set_question
-    @question = Question.find(params[:id])
+  def set_question_for_current_user
+    @question = current_user.questions.find(params[:id])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   validates :body, presence: true, length: {maximum: 280}
 
   belongs_to :user
+  belongs_to :author, class_name: 'User'
 
   def hidden?
     hidden
